b51b7a2a031f7b48a633da2a03873184e538c613
JSVasquez628 <vasquezstip28@gmail.com>
2024-10-24 15:12:35 -0500

    update produc and add users

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views import generic
 from products.forms import ProductForm
+from products.models import Product
 
 # Create your views here.
 class ProductFormView(generic.FormView):
@@ -12,3 +13,8 @@ class ProductFormView(generic.FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+    
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_products.html'
+    context_object_name = 'products'
