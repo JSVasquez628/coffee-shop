a92e6628ad2007cc8efde46cc25f10ee69232150
JSVasquez628 <vasquezstip28@gmail.com>
2024-11-07 13:49:39 -0500

    Add test and modify ''

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ProductListViewTest(TestCase):
+    def test_should_retourn_200(self):
+        url = reverse('list_products')
+        response = self.client.get(url)
+        breakpoint()
+        self.assertEqual(response.status_code, 200)
